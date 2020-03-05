f777c0b0e7637272db2db4bff90a1343ffee24d3
haukao <Olaussen99>
2020-03-05 11:47:30 +0100

    added gradleddasda

--- src/main/java/mainpackage/DBConn.java
+++ src/main/java/mainpackage/DBConn.java
@@ -13,10 +13,10 @@ public abstract class DBConn {
       // Class.forName("com.mysql.cj.jdbc.Driver"); when you are using MySQL 8.0.
       // Properties for user and password.
       Properties p = new Properties();
-      p.put("user", "myuser");
-      p.put("password", "mypassword");
+      p.put("user", "jonatbr_demo");
+      p.put("password", "12345678");
       //            conn = DriverManager.getConnection("jdbc:mysql://mysql.ansatt.ntnu.no/sveinbra_ektdb?autoReconnect=true&useSSL=false",p);
-      conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/ekt?autoReconnect=true&useSSL=false",p);
+      conn = DriverManager.getConnection("jdbc:mysql:mysql.stud.ntnu.no?autoReconnect=true&useSSL=false",p);
     } catch (Exception e)
     {
       throw new RuntimeException("Unable to connect", e);
